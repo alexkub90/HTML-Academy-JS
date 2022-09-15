dfa20b85b9d000d5d49696fe23031674356efcf5
alexkub90 <alexkub90@gmail.com>
2022-09-15 22:48:20 +0500

    format code

--- computerPrice.js
+++ computerPrice.js
@@ -2,41 +2,40 @@
 let processorPrice = {
     'i5': 5000,
     'i7': 10000
-  };
-  
-  let displayPrice = {
+};
+
+let displayPrice = {
     13: 5000,
     15: 10000
-  };
-  
-  let memoryPrice = {
+};
+
+let memoryPrice = {
     8: 3000,
     16: 4000
-  };
-  
-  // Функция которая "собирает" компьютер и считает стоимость
-  let buildComputer = function (memory, display, processor) {
+};
+
+// Функция которая "собирает" компьютер и считает стоимость
+let buildComputer = function (memory, display, processor) {
     let customComputer = {
-      basicPrice: 5000,
-      processor: processor,
-      display: display,
-      memory: memory,
-  
-      getDescription: function () {
-        return 'компьютер с процессором ' + this.processor + ', диагональю ' + this.display + ', оперативной памятью ' + this.memory;
-      },
-  
-      getPrice: function () {
-        return this.basicPrice + processorPrice[this.processor] + displayPrice[this.display] + memoryPrice[this.memory];
-      }
+        basicPrice: 5000,
+        processor: processor,
+        display: display,
+        memory: memory,
+
+        getDescription: function () {
+            return 'компьютер с процессором ' + this.processor + ', диагональю ' + this.display + ', оперативной памятью ' + this.memory;
+        },
+
+        getPrice: function () {
+            return this.basicPrice + processorPrice[this.processor] + displayPrice[this.display] + memoryPrice[this.memory];
+        }
     };
-  
+
     return customComputer;
-  };
-  
-  let myComputer = buildComputer(8, 13, 'i7');
-  console.log('В корзине ' + myComputer.getDescription() + ' стоимостью ' + myComputer.getPrice());
-  
-  let anotherComputer = buildComputer(16, 15, 'i5')
-  console.log('В корзине ' + anotherComputer.getDescription() + ' стоимостью ' + anotherComputer.getPrice())
-  
+};
+
+let myComputer = buildComputer(8, 13, 'i7');
+console.log('В корзине ' + myComputer.getDescription() + ' стоимостью ' + myComputer.getPrice());
+
+let anotherComputer = buildComputer(16, 15, 'i5')
+console.log('В корзине ' + anotherComputer.getDescription() + ' стоимостью ' + anotherComputer.getPrice())
